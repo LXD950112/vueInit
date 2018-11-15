13946ed3b683c6ad3d7537b354d90cf5aebbe326
lxd95112 <17858805411@163.com>
2018-11-15 11:24:08 +0800

    Init

---
+++ src/pages/index.vue
@@ -0,0 +1,32 @@
+<!--此处为1级页面-->
+<template>
+    <div class="index">
+        <main-head title="登录"></main-head>
+        <div class="con">首页内容</div>
+    </div>
+</template>
+
+<script>
+    import MainHead from "../components/Header";
+    export default {
+        name: 'index',
+        components: {MainHead},
+        data () {
+            return {
+                msg: 'Welcome to Your Vue.js App'
+            }
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+    .index {
+        width: 750px;
+        background: red;
+    }
+    .con {
+        height: 50px;
+        background: url("../assets/common/title_back.png") no-repeat;
+    }
+</style>
